baab4b6bcda67c599c2179635676b05f1a24844f
miyauchi <miyauchi@localhost.(none)>
2012-08-26 15:53:31 +0900

    first commit

---
+++ speed-test.js
@@ -0,0 +1,35 @@
+(function($){
+
+$('#speed-test-start').click(function(){
+    $('#booster-stopped').html('');
+    $('#booster-running').html('');
+    var d = new Date();
+    var path = content_url+'/plugins/wpbooster-speed-test/test-images/img_r{0}_c{1}.png?ver=';
+    var booster = 'http://demo.wpbooster.net/test-images/default/img_r{0}_c{1}.png?ver=';
+    for (var i=1; i<=10; i++) {
+        for (var j=1; j<=10; j++) {
+            var img = $('<img />');
+            img.attr('src', $.sprintf(path, i, j)+d.getTime());
+            img.attr('width', "25");
+            img.attr('height', "25");
+            $('#booster-stopped').append(img);
+            var img2 = $('<img />');
+            img2.attr('src', $.sprintf(booster, i, j)+d.getTime());
+            img2.attr('width', "25");
+            img2.attr('height', "25");
+            $('#booster-running').append(img2);
+        }
+    }
+});
+
+$.extend({
+    sprintf: function(format) {
+        for (var i = 1; i < arguments.length; i++) {
+            var reg = new RegExp('\\{' + (i - 1) + '\\}', 'g');
+            format = format.replace(reg, arguments[i]);
+        }
+        return format;
+    }
+});
+
+})(jQuery); // EOF
---
+++ wpbooster-speed-test.php
@@ -0,0 +1,74 @@
+<?php
+/*
+Plugin Name: WP Booster Speed Test
+Description: WP Booster Speed Test
+Author: Digital Cube Co.,Ltd (Takayuki Miyauchi)
+Domain Path: /languages
+Text Domain: wpbooster-speed-test
+*/
+
+
+new WPBoosterDemo();
+
+class WPBoosterDemo {
+
+function __construct()
+{
+    add_action("plugins_loaded", array(&$this, "plugins_loaded"));
+    add_action("wp_enqueue_scripts", array(&$this, "wp_enqueue_scripts"));
+    add_shortcode("wpbooster_speed_test", array(&$this, 'shortcode'));
+}
+
+public function plugins_loaded()
+{
+    load_plugin_textdomain(
+        'wpbooster-speed-test',
+        false,
+        dirname(plugin_basename(__FILE__)).'/languages'
+    );
+}
+
+public function wp_enqueue_scripts()
+{
+    wp_enqueue_style(
+        'wpbooster-speed-test',
+        plugins_url('style.css', __FILE__),
+        array(),
+        filemtime(dirname(__FILE__).'/style.css')
+    );
+}
+
+public function shortcode()
+{
+
+    wp_enqueue_script(
+        'wpbooster-speed-test',
+        plugins_url('speed-test.js', __FILE__),
+        array('jquery'),
+        filemtime(dirname(__FILE__).'/speed-test.js'),
+        true
+    );
+
+    add_action("wp_footer", array(&$this, "wp_footer"));
+
+    $div = '<div id="%s"></div>';
+    $html = '<div id="wpbooster-speed-test">';
+    $html .= sprintf($div, 'booster-stopped');
+    $html .= sprintf($div, 'booster-running');
+    $html .= '</div>';
+
+    $html .= '<a id="speed-test-start" class="btn-fire">Booster Fire!</a>';
+    $html .= '<p id="get-booster"><a href="http://ja.wpbooster.net/">Get WP Booster</a></p>';
+    return $html;
+}
+
+
+public function wp_footer()
+{
+    $script = "<script type=\"text/javascript\">var content_url = '%s';</script>";
+    printf($script, esc_html(content_url()));
+}
+
+}
+
+// EOF
